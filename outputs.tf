6e06bad7b688d724f10904f96e4690c1c154598a
vijay <ellaboina>
2024-10-15 16:05:49 +0530

    terraform peering

---
+++ outputs.tf
@@ -0,0 +1,12 @@
+output "vpc_id" {
+    value = aws_vpc.main.id
+}
+# output "az_info" {
+#     value = data.aws_availability_zones
+# }
+# output "default_vpc_info"{
+#     value = data.aws_vpc.default
+# }
+# output "main_route_table_info" {
+#     value = data.aws_route_table.main
+# }
